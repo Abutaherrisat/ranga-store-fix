b9fa2767d0188cc441d9c2a4433257cb0ad26d35
Abutaherrisat <abutaherrisat@gmail.com>
2021-09-13 15:58:18 +0600

    color added

--- js/app.js
+++ js/app.js
@@ -16,10 +16,10 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
+      <h2>Price:  ${product.price}</h2>
       <p>Rating: ${product.rating.rate}</p>
       <p>Total Rating: ${product.rating.count}</p>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-warning">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
